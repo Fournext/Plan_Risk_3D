cb9b8bf9d2f0b8a77f5196ed33c4cb0430391052
Fournext <sebastianzc04@gmail.com>
2025-09-03 00:52:55 -0400

    correccion y generara requerimientos

--- back_plan_risk_3d/api_plan_risk_3d/settings.py
+++ back_plan_risk_3d/api_plan_risk_3d/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'plan_risk_db',      # nombre de tu base de datos
         'USER': 'postgres',          # tu usuario de PostgreSQL
-        'PASSWORD': '8960406',   # la contraseña de ese usuario
+        'PASSWORD': '071104',   # la contraseña de ese usuario
         'HOST': 'localhost',         # o la IP del servidor si es remoto
         'PORT': '5432',              # puerto por defecto de PostgreSQL
     }
