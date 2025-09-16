2d11f4de07a7ae6409f14899624e3c9df9a885b9
Henry123JA <positronsito1.62@gmail.com>
2025-09-15 21:09:28 -0400

    conservando mis ajustes setting antes del merge

--- back_plan_risk_3d/api_plan_risk_3d/settings.py
+++ back_plan_risk_3d/api_plan_risk_3d/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'plan_risk_db',      # nombre de tu base de datos
         'USER': 'postgres',          # tu usuario de PostgreSQL
-        'PASSWORD': '071104',   # la contraseña de ese usuario
-        'HOST': 'postgres',         # o la IP del servidor si es remoto
+        'PASSWORD': '8960406',   # la contraseña de ese usuario
+        'HOST': 'localhost',         # o la IP del servidor si es remoto
         'PORT': '5432',              # puerto por defecto de PostgreSQL
     }
 }
