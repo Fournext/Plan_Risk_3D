13cdd58b7ebc3c1ce6067dd8b3dadf4f3e0dee9a
Fournext <sebastianzc04@gmail.com>
2025-11-21 02:56:39 -0400

    fix: error libreria

--- back_plan_risk_3d/api_plan_risk_3d/settings.py
+++ back_plan_risk_3d/api_plan_risk_3d/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         'NAME': 'plan_risk_db',      # nombre de tu base de datos
         'USER': 'postgres',          # tu usuario de PostgreSQL
         'PASSWORD': '071104',   # la contraseña de ese usuario
-        'HOST': 'postgres',         # o la IP del servidor si es remoto
+        'HOST': 'localhost',         # o la IP del servidor si es remoto
         'PORT': '5432',              # puerto por defecto de PostgreSQL
     }
 }
