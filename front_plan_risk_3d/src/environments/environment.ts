d7f40f88adf01b8aeef425b6f75258070780bf65
Fournext <sebastianzc04@gmail.com>
2025-11-21 01:23:37 -0400

    Desp 1

--- front_plan_risk_3d/src/environments/environment.ts
+++ front_plan_risk_3d/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  endpoint: 'http://localhost:8000/', //url para local
-  //endpoint: 'https://django-sw1.fournext.me/', //url para despiegue
+  //endpoint: 'http://localhost:8000/', //url para local
+  endpoint: 'https://django-sw1.fournext.me/', //url para despiegue
   cloudinary: 'https://api.cloudinary.com/v1_1'
 }
