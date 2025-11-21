63ffe9dd13c651d7d3edd98f7b672389396a2dda
Fournext <sebastianzc04@gmail.com>
2025-11-21 02:00:48 -0400

    fix: error de database

--- back_plan_risk_3d/api_plan_risk_3d/settings.py
+++ back_plan_risk_3d/api_plan_risk_3d/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'plan_risk_db',      # nombre de tu base de datos
         'USER': 'postgres',          # tu usuario de PostgreSQL
-        'PASSWORD': '6305913',   # la contraseña de ese usuario
+        'PASSWORD': '071104',   # la contraseña de ese usuario
         'HOST': 'postgres',         # o la IP del servidor si es remoto
         'PORT': '5432',              # puerto por defecto de PostgreSQL
     }
